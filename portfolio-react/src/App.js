885c42c83ff59a7c02bebe58a717fc0150aa686d
Ryan Anderson <ryan0425@msn.com>
2021-05-19 09:47:46 -0700

    updated App.js but still needs work

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,11 @@
 import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./components/home";
 import About from "./components/about";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Projects from "./components/projects";
 import Resume from "./components/resume";
+
 import './App.css';
 
 
@@ -15,18 +17,17 @@ function App() {
         <Home />
 
       </Route>
-      <Main>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/projects">
-          <About />
-        </Route>
-        <Route exact path="/resume">
-          <About />
-        </Route>
 
-      </Main>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/projects">
+        <About />
+      </Route>
+      <Route exact path="/resume">
+        <About />
+      </Route>
+
       <Footer />
     </Router>
   );
