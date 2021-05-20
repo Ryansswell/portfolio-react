a2821e8c4232a8d45e356e1fbef43988be8469a3
Ryan Anderson <ryan0425@msn.com>
2021-05-19 23:07:16 -0700

    was able to get the About me portion to show up!

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Projects from "./components/projects";
 import Resume from "./components/resume";
+import Headshot from './assets/Headshot.jpg'
 import './App.css';
 
 
@@ -14,9 +15,9 @@ function App() {
         <Router>
             <div>
                 <Navbar />
-                {/* <header className="py-5 bg-image-full" style={{ "background-image: url('assets/maui2.jpeg')"}} >
-                    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }} />
-                    <img class="img-fluid d-block mx-auto" src="assets/Portfolio Headshot.jpg" alt="Headshot" style={{ "width:250px, height:250px"}} />
+
+                <header className="py-5 bg-image-full" >
+                    <img class="img-fluid d-block mx-auto" src={Headshot.jpg} alt="Headshot" />
                 </header>
 
                 <section class="py-5">
@@ -24,6 +25,11 @@ function App() {
 
                         <h1>About Me</h1>
                         <p class="lead">Life is the best when you can enjoy the journey!</p>
+                        <p>I am on the journey of becoming a Software Devoloper! I am in the final stages of finishing up UCSD's Coding Bootcamp.
+                        I can't believe how much I have learned so far and how bad my head hurts from studying daily. I have been intrigued with Coding and Software development for several years but now finally taking the leap to becoming knowledgle and experienced.
+                        I know I can do anything I put my mind to and excited for the endless learning in this great field!
+                        
+                        </p>
                         <p>I am so fortunate to call San Diego my home! Growing up in Colorado, I have always appreciated the outdoors and
                         making great friends.
                         Surfing is my favorite hobby and favorite workout. I dream about surfing when I am not coding! I have been
@@ -32,27 +38,21 @@ function App() {
                         Traveling has always been one of my favorite hobbies, along with experiencing new cultures and people!
 
                         Living out true passions and making slight improvements everyday is amazing.
-      </p>
+                        </p>
                     </div>
                 </section>
 
 
-                <div class="py-5 bg-image-full" style="background-image: url('assets/IMG_4019.jpg');">
-                    <div style="height: 200px;"></div>
-                </div> */}
-                <Route exact path="/about">
-                    <About />
-                </Route>
-                <Route exact path="/projects">
-                    <Projects />
-                </Route>
-                <Route exact path="/resume">
-                    <Resume />
-                </Route>
-
+                <div class="py-5 bg-image-full" >
+                    <div ></div>
+                </div>
+                <div>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                </div>
             </div>
-        </Router >
-
+        </Router>
     );
 }
 
